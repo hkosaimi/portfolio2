4f30b933ba7a1ba86e9e2add1a4b0f81689c1f01
Hussain <hkosaimi@gmail.com>
2024-01-31 19:35:07 +0300

    new

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ let articles = document.getElementById("articles");
 let linkArr = [portfolio2, about, articles];
 
 for (let i = 0; i < linkArr.length; i++) {
-  if (location.pathname === `/${linkArr[i].id}.html`) {
+  if (location.pathname === `/portfolio2/${linkArr[i].id}.html`) {
     linkArr[i].style.color = "red";
   }
 }
