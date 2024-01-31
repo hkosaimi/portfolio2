b938d74747dc71d95ed7fc8781544455f8e4594e
Hussain <hkosaimi@gmail.com>
2024-01-31 19:53:44 +0300

    new

--- main.js
+++ main.js
@@ -1,13 +1,21 @@
-let portfolio2 = document.getElementById("index");
+let index = document.getElementById("index");
 let about = document.getElementById("about");
 let articles = document.getElementById("articles");
 
 let linkArr = [portfolio2, about, articles];
 
-for (let i = 0; i < linkArr.length; i++) {
+/* for (let i = 0; i < linkArr.length; i++) {
   if (location.pathname === `/portfolio2/${linkArr[i].id}.html`) {
     linkArr[i].style.color = "red";
   }
+} */
+
+if (location.pathname === `/portfolio2`) {
+  index.style.color = "red";
 }
-console.log(location.pathname);
-console.log(linkArr[2].id);
+if (location.pathname === `/portfolio2/about.html`) {
+  about.style.color = "red";
+}
+if (location.pathname === `/portfolio2/articles.html`) {
+  articles.style.color = "red";
+}
